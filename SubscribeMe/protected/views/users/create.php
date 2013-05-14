79d924836cf920516b8bbde3284c65ed6a3ae0e0
GRAAFYME <graaf.yme91@hotmail.com>
2013-05-14 10:10:01 +0200

    config/main.php aangepast voor de database connectie. Model, Crud en View voor users gemaakt.

---
+++ SubscribeMe/protected/views/users/create.php
@@ -0,0 +1,18 @@
+<?php
+/* @var $this UsersController */
+/* @var $model users */
+
+$this->breadcrumbs=array(
+	'Users'=>array('index'),
+	'Create',
+);
+
+$this->menu=array(
+	array('label'=>'List users', 'url'=>array('index')),
+	array('label'=>'Manage users', 'url'=>array('admin')),
+);
+?>
+
+<h1>Create users</h1>
+
+<?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
