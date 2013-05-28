e8c62231ded654ffb6898a12d7eec9af38404b18
Roer1200 <wotuerroersma@gmail.co>
2013-05-28 11:04:58 +0200

    1. Inschrijf functie toegevoegd

--- SubscribeMe/application/controllers/subscribe.php
+++ SubscribeMe/application/controllers/subscribe.php
@@ -4,28 +4,52 @@ class Subscribe extends CI_Controller {
 	public function __construct()
 	{
 		parent::__construct();
-		$this->load->model('page_model');
-		$this->load->library('menu');	
+		$this->load->helper('file');
+		$this->load->library('menu');
+		$this->load->model('xmlparser_model');
+		$this->load->model('subscribe_model');
 	}
 
-	public function index()
+	public function getall()
 	{
-		$data['subscribe_item'] = $this->page_model->get_subscribe();
+		$menu = new Menu;
+		$data['menu'] = $menu->show_menu();
+		$data['xml'] = $this->subscribe_model->getall();
 
-		if (empty($data['subscribe_item'])) // News item DOESN'T exitst!
-		{
-			show_404();
-		}
-		else // News item DOES exitst!
-		{	
-			$menu = new Menu;
+		$this->load->view('templates/frontend/header',$data);
+		$this->load->view('courses/courses',$data);
+		$this->load->view('templates/frontend/footer');
 
-			$data['menu'] = $menu->show_menu();
-			$data['title'] = $data['subscribe_item']['title'];
-
-			$this->load->view('templates/frontend/header', $data);
-			$this->load->view('subscribe/index', $data);
-			$this->load->view('templates/frontend/footer');
-		}
 	}
-}
+ 	public function get($year)
+ 	{
+
+ 		
+		$menu = new Menu;
+		$data['menu'] = $menu->show_menu();
+ 		
+
+ 		$data['xml'] = $this->subscribe_model->getxml($year);
+
+ 		$this->load->view('templates/frontend/header',$data);
+		$this->load->view('courses/courses',$data);
+		$this->load->view('templates/frontend/footer');
+ 	}
+
+ 	public function course($id)
+ 	{
+ 		$menu = new Menu;
+		$data['menu'] = $menu->show_menu();
+
+		$data['xml'] = $this->subscribe_model->getcourse($id);
+
+		$this->load->view('templates/frontend/header',$data);
+		$this->load->view('courses/course',$data);
+		$this->load->view('templates/frontend/footer');
+ 	}
+
+ 	public function signup($id)
+ 	{
+ 	  $this->subscribe_model ->signup($id);
+ 	}
+ }
