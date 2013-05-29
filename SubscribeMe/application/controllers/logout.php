f59b97b2cab90bcdfa64212ed7cde558ada72986
GRAAFYME <graaf.yme91@hotmail.com>
2013-05-29 12:42:21 +0200

    Eerste stap van het LDAP login systeem...

--- SubscribeMe/application/controllers/logout.php
+++ SubscribeMe/application/controllers/logout.php
@@ -1,10 +1,23 @@
-<?php 
-//this function kills the current session and redirects the user to the login controller
-class Logout extends CI_Controller {
+<?php
+
+class Logout extends CI_Controller 
+{
+	function __construct()
+	{
+		parent::__construct();
+	}
 
 	function index()
 	{
-		$this->session->sess_destroy();
-		redirect('login');
+		if($this->session->userdata('logged_in'))
+		{
+			$this->session->unset_userdata('logged_in');
+			$this->session->sess_destroy();
+			redirect('login');
+		}
+		else
+		{
+			redirect('login',);
+		}
 	}
 }
